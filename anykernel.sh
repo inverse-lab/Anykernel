fd9a5dbade32cbb1bcc4ce14ecbf5ca9cc81c476
zaidannew <118640080+zaidannew@users.noreply.github.com>
2023-02-01 09:33:55 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= - Friday, 30 December 2022
+kernel.string= TheDeepestShadow Kernel - zaidannn7
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -31,26 +31,6 @@ patch_vbmeta_flag=auto;
 ### AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
-# Print LOGO
-ui_print "    ____ _____                               ";
-ui_print "   / ___|  ___|                              ";
-ui_print "  | |  _| |       Stay Simple,               ";
-ui_print "  | |_| |  _|                 Stay Naive     ";
-ui_print "   \____|_|                                  ";
-ui_print "   ____  ____   ___      _ _____ ____ _____  ";
-ui_print "  |  _ \|  _ \ / _ \    | | ____/ ___|_   _| ";
-ui_print "  | |_) | |_) | | | |_  | |  _|| |     | |   ";
-ui_print "  |  __/|  _ <| |_| | |_| | |__| |___  | |   ";
-ui_print "  |_|   |_| \_ \___/ \___/|_____\____| |_|   ";
-ui_print "                                             ";
-sleep 1
-ui_print "                 Directed by:                ";
-ui_print "                                             ";
-ui_print "                 Mhmmd Fdlyas                ";
-ui_print "                      &                      ";
-ui_print "                 Zecho Satria                ";
-ui_print "                                             ";
 
 ## AnyKernel boot install
 dump_boot;
